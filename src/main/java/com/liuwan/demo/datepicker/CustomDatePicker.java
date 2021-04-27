786255bfeb4da05fbb76bf70becbbc0b55902643
CoderGxw <xuewei_G@163.com>
2021-04-27 11:28:39 +0800

    Update CustomDatePicker.java
    弹出框时，隐藏导航条

--- src/main/java/com/liuwan/demo/datepicker/CustomDatePicker.java
+++ src/main/java/com/liuwan/demo/datepicker/CustomDatePicker.java
@@ -116,6 +116,7 @@ public class CustomDatePicker implements View.OnClickListener, PickerView.OnSele
 
         Window window = mPickerDialog.getWindow();
         if (window != null) {
+            dynamicHideNavigationBar(window);
             WindowManager.LayoutParams lp = window.getAttributes();
             lp.gravity = Gravity.BOTTOM;
             lp.width = WindowManager.LayoutParams.MATCH_PARENT;
@@ -632,6 +633,30 @@ public class CustomDatePicker implements View.OnClickListener, PickerView.OnSele
             mDpvHour.onDestroy();
             mDpvMinute.onDestroy();
         }
+    }
+    
+    public  void dynamicHideNavigationBar(final Window window) {
+        //保持布局状态
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE|
+                //布局位于状态栏下方
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|
+                //全屏
+                View.SYSTEM_UI_FLAG_FULLSCREEN|
+                //隐藏导航栏
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        if (Build.VERSION.SDK_INT>=19){
+            uiOptions |= 0x00001000;
+        }else{
+            uiOptions |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+        }
+        window.getDecorView().setSystemUiVisibility(uiOptions) ;
+        window.getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                dynamicHideNavigationBar(window);
+            }
+        });
     }
 
 }
